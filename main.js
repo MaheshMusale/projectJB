2947aeb145b714f8eeba9bd248ed9ee856d653c4
MaheshMusale <31409795+MaheshMusale@users.noreply.github.com>
2020-08-11 04:21:36 +0530

    Update main.js

--- main.js
+++ main.js
@@ -10,6 +10,6 @@ app.use(index);
 app.use(express.static("G:/Database/projectJB" + '/public'));
 
 
-app.listen(3000, process.env.IP, function(){
+app.listen(process.env.PORT, process.env.IP, function(){
    console.log("The Server Has Started!");
 });
